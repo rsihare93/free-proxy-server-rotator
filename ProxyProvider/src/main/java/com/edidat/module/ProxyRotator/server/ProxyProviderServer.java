0b9cc8c242a743cc7bd67defc1635207304587f1
Rohit Ihare <rsihare93@gmail.com>
2018-09-21 09:15:51 +0530

    Initial project structure

---
+++ ProxyProvider/src/main/java/com/edidat/module/ProxyRotator/server/ProxyProviderServer.java
@@ -0,0 +1,17 @@
+package com.edidat.module.ProxyRotator.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProxyProviderServer 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+    
+    public static void start() {
+    	
+    }
+}
