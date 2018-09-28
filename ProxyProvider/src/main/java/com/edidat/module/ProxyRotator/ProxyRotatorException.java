ed5cc98997cc229a9b75fd89eb522b39c9bd5907
rohit.ihare <rsihare93@gmail.com>
2018-09-28 11:49:28 +0530

    Completed : TCP Server implementation.

---
+++ ProxyProvider/src/main/java/com/edidat/module/ProxyRotator/ProxyRotatorException.java
@@ -0,0 +1,33 @@
+package com.edidat.module.ProxyRotator;
+
+public class ProxyRotatorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5239482294140790908L;
+
+	public ProxyRotatorException() {
+		super();
+	}
+
+	public ProxyRotatorException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ProxyRotatorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ProxyRotatorException(String message) {
+		super(message);
+	}
+
+	public ProxyRotatorException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+}
--- ProxyProvider/src/main/java/com/edidat/module/ProxyRotator/server/ProxyProviderServer.java
+++ ProxyProvider/src/main/java/com/edidat/module/ProxyRotator/server/ProxyProviderServer.java
@@ -1,40 +1,85 @@
 package com.edidat.module.ProxyRotator.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.helper.StringUtil;
 
 import com.edidat.module.ProxyRotator.Constants;
+import com.edidat.module.ProxyRotator.ProxyRotator;
+import com.edidat.module.ProxyRotator.ProxyRotatorException;
 
 /**
- * Hello world!
- *
+ * @author rohit.ihare
+ * {@link ProxyProviderServer} is the server application which provides the free proxies available across Internet with the help of free-APIs and web scrapers.
+ * It servers the client request over tcp connection and communicates using json. 
  */
-public class ProxyProviderServer 
-{
+public class ProxyProviderServer {
 	public static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
-	
-    public static void main( String[] args )
-    {
-       
-    }
-    
-    public static void start() {
-    	
-    }
+	private static final Logger logger = LogManager.getLogger(ProxyProviderServer.class);
 
-    
-    public static void clientConnectionHandler () throws Exception {
-    	String port = System.getenv(Constants.PROXY_PROVIDER_SERVER_PORT);
-    	if(StringUtil.isBlank(port)) {
-    		throw new Exception("Server port is not defined");
-    	}
-    	ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port));
-    	threadPool.submit(new ClientConnectionHandler(serverSocket.accept()));
-    	
-    }
-    
+	public static void main(String[] args) {
+		try {
+			// starts the server.
+			start();
+		} catch (ProxyRotatorException e) {
+			logger.fatal(e.getMessage());
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * Init the proxy queue and start filling it with extracted proxies
+	 * Also starts the tcp server to serve client requests.
+	 * @throws Exception
+	 */
+	public static void start() throws ProxyRotatorException {
+		logger.info("Starting server ...");
+		ProxyRotator.getInstance().init();
+		clientRequestServer();
+	}
+
+	
+	/**
+	 * It starts the tcp server and accepts proxy requests from client in json format.
+	 * @throws ProxyRotatorException
+	 */
+	public static void clientRequestServer() throws ProxyRotatorException {
+		String port = System.getenv(Constants.PROXY_PROVIDER_SERVER_PORT);
+		if (StringUtil.isBlank(port)) {
+			throw new ProxyRotatorException("Server port is not defined");
+		}
+		ServerSocket serverSocket = null;
+		boolean shutdown = false;
+		try {
+			/* TCP Server starts at specified port and starts accepting clients */
+			serverSocket = new ServerSocket(Integer.parseInt(port));
+		} catch (NumberFormatException | IOException e1) {
+			logger.error(e1.getMessage());
+			throw new ProxyRotatorException(e1.getMessage(), e1);
+		}
+		logger.info("Server has been started!!!");
+		while (!shutdown) {
+			try {
+				/* TCP Server accepts clients and create separate thread for handling clients requests */
+				threadPool.submit(new ClientConnectionHandler(serverSocket.accept()));
+			} catch (IOException e) {
+				shutdown = true;
+				logger.error(e.getMessage());
+				throw new ProxyRotatorException(e.getMessage(), e);
+			} finally {
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					logger.error(e.getMessage());
+					throw new ProxyRotatorException(e.getMessage(), e);
+				}
+			}
+		}
+	}
+
 }
